dcd0f5da35eddfed2e17f7d8a32200e6f36dcedf
MafariT <ahmadfadlan@proton.me>
2025-10-30 01:07:12 +0700

    weird fix for cookies

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -9,14 +9,7 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async getAll() {
-          return (await cookieStore).getAll().map(({ name, value }) => ({ name, value }))
-        },
-        async setAll(cookiesToSet) {
-          for (const { name, value, options } of cookiesToSet) {
-            (await cookieStore).set(name, value, options)
-          }
-        },
+        getAll: async () => (await cookieStore).getAll()
       },
     }
   )
