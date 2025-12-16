f025fd4a42ab432ff201cdfecb3d3a0494158e8b
MafariT <ahmadfadlan@proton.me>
2025-12-16 09:44:50 +0700

    increase body limit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['pdfkit'],
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '12mb',
+    },
+  },
 };
 
 export default nextConfig;
